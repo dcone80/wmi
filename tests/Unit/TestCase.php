31bc05c54b5149c14af1fc13e4e0e7f2b2e7ebfc
Steve Bauman <steven_bauman@outlook.com>
2015-06-30 15:40:44 -0400

    Update composer.json with dev dependencies

--- tests/Unit/TestCase.php
+++ tests/Unit/TestCase.php
@@ -2,7 +2,12 @@
 
 namespace Stevebauman\Wmi\Tests\Unit;
 
-class TestCase
-{
+use PHPUnit_Framework_TestCase;
 
+class TestCase extends PHPUnit_Framework_TestCase
+{
+    protected function setUp()
+    {
+
+    }
 }
