b416d141de2bc93b2b0db71c4e1417851dad98cc
Steve Bauman <steven_bauman@outlook.com>
2020-02-13 10:55:23 -0500

    Update TestCase.php

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,9 @@
 
 namespace Stevebauman\Wmi\Tests;
 
-use PHPUnit_Framework_TestCase;
+use PHPUnit\Framework\TestCase as BaseTestCase;
 
-class TestCase extends PHPUnit_Framework_TestCase
+class TestCase extends BaseTestCase
 {
     //
 }
